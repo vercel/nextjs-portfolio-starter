b4a4920bba7e60567cf720e65d7525c2f5b0cb8f
jimuel.palaca <jimuelpalaca@gmail.com>
2021-10-02 00:18:51 +0800

    Add security headers config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,41 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+
+module.exports = withNextra({
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: 'pgrade-insecure-requests'
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=2592000;includeSubdomains'
+                    },
+                    {
+                        key: 'X-Xss-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin'
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'geolocation=(self)'
+                    }
+                ]
+            }
+        ];
+    }
+})
