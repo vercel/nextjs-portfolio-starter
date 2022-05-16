e4e129ecd968ff987cd66b2d4f1a1d913e426a39
MiniNaim <mininaim@gmail.com>
2022-05-16 01:43:01 +0100

    Edit the copyright symbol

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> &copy; Your Name.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
